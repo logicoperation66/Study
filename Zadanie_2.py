b15e3083f39bfec2d161d9020e6031bd7bd075d4
Adam Waw <adamwawrzyniak5@gmail.com>
2021-06-24 22:40:35 +0200

    Refactor, usunięcię listy i skrócenie kodu. Feel stupid -.-

--- Zadanie_2.py
+++ Zadanie_2.py
@@ -3,12 +3,9 @@
 """Funkcja do obliczania sześcianu sumy n liczb"""
 
 number = int(input('Podaj liczbę :'))
-summ = []
+wynik = 0
 
 for x in range(1, number+1):
-    qube = x**3
-    summ.append(qube)
-wynik = sum(summ)
+    wynik += x*x*x
 print(wynik)
-## To jest jakaś abstrakcja... ale to pierwszy pomysł jaki mi wpadł głowy...
-# DO POPRAWY
+
