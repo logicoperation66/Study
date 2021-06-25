6927eddf61da015564eadf2654a76e43232d1b6b
Adam Waw <adamwawrzyniak5@gmail.com>
2021-06-26 00:51:02 +0200

    Działa

--- Zadanie_4.py
+++ Zadanie_4.py
@@ -18,12 +18,13 @@ def printing_duplicates():
     #Pętla for iteruję wszystkie znaki naszego stringa i sprawdza czy któryś
     # znak występuje dwa lub więcej razy.
     for digit in dictionary:
-        if leter.count(digit) >= 2:
+        if leter.count(digit) > 1:
             print(digit)
 
 printing_duplicates()
 
-# Pomysł drugi po doedukowaniu się w internecie
+
+# Pomysł drugi po doedukowaniu się w internecie. Output wpada do słownika
 
 def printing_duplicates2():
     text = input('Wpisz ciąg znaków :')
--- Zadanie_5.py
+++ Zadanie_5.py
@@ -3,21 +3,22 @@
 # arr[].
 
 """Funkcja do wypisywania indexu szukanego elementu w liscie"""
+def szukanko():
+    #Tworze dowolną liste liczb
 
-#Tworze losowa liste
+    lista = [10,11,13,16,17,95,45,68,48,33,51,47,]
+    szukana = int(input('Podaj jakiej liczby szukasz :'))
 
-lista = [10,11,13,16,17,95,45,68,48,33,51,47,]
-szukana = int(input('Podaj jakiej liczby szukasz :'))
+    #lista = list(set(lista))
 
-#lista = list(set(lista))
+    print(f'Twoja lista wygląda następująco {lista}')
+    print(f"Szukamy elementu [{szukana}]")
+    for element in lista:
+        if szukana == element:
+            print(f"{szukana} znajduję się na liście pod indexem "
+                  f"{lista.index(szukana)}")
+            break
+    else:
+        print(f'Szukanej {szukana} nie ma na liście.')
 
-print(f'Twoja lista wygląda następująco {lista}')
-print(f"Szukamy elementu [{szukana}]")
-for element in lista:
-    if szukana == element:
-        print(f"{szukana} znajduję się la liście pod indexem "
-              f"{lista.index(szukana)}")
-        break
-else:
-    print('Szukanej nie ma na liście')
-
+szukanko()
