9bba79195c5b0c4d348f9dd3be02556dc7fc5e7e
Adam Waw <adamwawrzyniak5@gmail.com>
2021-06-25 21:53:19 +0200

    Przepisanie skryptu do funkcji

--- Zadanie_2.py
+++ Zadanie_2.py
@@ -2,10 +2,12 @@
 
 """Funkcja do obliczania sześcianu sumy n liczb"""
 
-number = int(input('Podaj liczbę :'))
-wynik = 0
 
-for x in range(1, number+1):
-    wynik += x*x*x
-print(wynik)
+def qube_sum():
+    wynik = 0
+    number = int(input('Podaj liczbę :'))
+    for x in range(1, number+1):
+        wynik += x*x*x
+    print(wynik)
 
+qube_sum()
