012db7528bcc56cd3783246ed004c7d5ac734637
Adam Waw <adamwawrzyniak5@gmail.com>
2021-06-25 21:56:38 +0200

    Dodanie skryptu do funkcji

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -3,28 +3,32 @@
 """Prosta aplikacja do liczenia należnego podatku z funkcją sprawdzania
 poprawności wprowadzonych danych"""
 
-pr1 = 3091
-pr2 = 85528
-st1 = 0.18
-st2 = 0.32
 
-prompt1 = "Podaj jaki masz dochód"
+def tax():
+    pr1 = 3091
+    pr2 = 85528
+    st1 = 0.18
+    st2 = 0.32
 
-print(prompt1)
+    prompt1 = "Podaj jaki masz dochód"
 
-while True:
-    try:
-        bejmy = float(input('zł :'))
-    except ValueError:
-        print('Podałeś niewłaściwe dane. Wpisz kwote')
+    print(prompt1)
+    while True:
+        try:
+            bejmy = float(input('zł :'))
+        except ValueError:
+            print('Podałeś niewłaściwe dane. Wpisz kwote.')
+        else:
+            break
+
+    if pr1 < bejmy < pr2:
+        ciezar = st1*(bejmy-pr1)
+        print(f"Musisz odprowadzić {round(ciezar, 2)} zł podatku")
+    elif bejmy > pr2:
+        ciezar2 = st1*(pr2-pr1)+st2*(bejmy-pr2)
+        print(f"Musisz odprowadzić {round(ciezar2, 2)} zł podatku")
     else:
-        break
+        print('Jesteś zwolniony z podatku "szczęsciarzu"')
 
-if pr1 < bejmy < pr2:
-    ciezar = st1*(bejmy-pr1)
-    print(f"Musisz odprowadzić {round(ciezar, 2)} zł podatku")
-elif bejmy > pr2:
-    ciezar2 = st1*(pr2-pr1)+st2*(bejmy-pr2)
-    print(f"Musisz odprowadzić {round(ciezar2, 2)} zł podatku")
-else:
-    print('Jesteś zwolniony z podatku "szczęsciarzu"')
+
+tax()
