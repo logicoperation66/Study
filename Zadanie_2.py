bc4e9e8ce3b3f235b429a5aec100346d5a10a7fe
Adam Waw <adamwawrzyniak5@gmail.com>
2021-06-26 17:04:05 +0200

    Add speedtest to see how bad qube_sum2 is

--- Zadanie_2.py
+++ Zadanie_2.py
@@ -1,23 +1,48 @@
 #Zad.2
 
 """Funkcja do obliczania sześcianu sumy n liczb"""
+import datetime
+import time
 
+#Pierwszy pomysł
 def qube_sum():
     wynik = 0
     number = int(input('Podaj liczbę :'))
+    start_time = datetime.datetime.now()
     for x in range(1, number+1):
         wynik += x*x*x
-    print(wynik)
+    time_dif = datetime.datetime.now() - start_time
+    exec_time = time_dif.total_seconds()
+    print(f'Wynik funkcji = {wynik}')
+    print(f'Komputer obliczył funkcję w czasie {exec_time}s')
 
-qube_sum()
-
-#Pomysł drugi. A raczej pierwszy(gorszy)
-
+#Pomysł drugi. A raczej pierwszy pomysł(gorszy)
 def qube_sum2():
     number = int(input('Podaj liczbę :'))
     lista = []
+    start_time = datetime.datetime.now()
     for x in range(1, number+1):
         lista.append(x**3)
-    print(sum(lista))
+    time_dif = datetime.datetime.now() - start_time
+    exec_time = time_dif.total_seconds()
+    print(f'Wynik funkcji = {sum(lista)}')
+    print(f'Komputer obliczył funkcję w czasie {exec_time}s')
 
-qube_sum2()
+# Licznik czasu wykonywania działań
+
+def program_selector(program):
+
+    if program == 1:
+        qube_sum()
+    elif program == 2:
+        qube_sum2()
+
+### After test ... Program 2 zjada potężne ilości pamięci i jest znacznie
+# wolniejszy.
+
+prompt1 = "Wybierz który program chcesz użyć (1,2) :"
+
+program = int(input(prompt1))
+
+program_selector(program)
+
